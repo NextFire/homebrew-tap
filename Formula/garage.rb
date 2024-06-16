bdf305149098a2bc3b82dd7cdd8befd4d7f3e21a
Nam Vu <git@yuru.moe>
2024-06-16 10:36:34 +0000

    garage: add 1.0.0 bottle.

--- Formula/garage.rb
+++ Formula/garage.rb
@@ -6,6 +6,13 @@ class Garage < Formula
   license "AGPL-3.0-or-later"
   head "https://git.deuxfleurs.fr/Deuxfleurs/garage.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/NextFire/homebrew-tap/releases/download/garage-1.0.0"
+    sha256 cellar: :any,                 arm64_sonoma: "839a448bb378c3a1274d9f6fa19499376ac6711e58093fb754cde1e9d96e7117"
+    sha256 cellar: :any,                 ventura:      "6856c87b0463894c72e6a0be2fceca129d749ec31c134b124d9d9bc6c63928b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "27036921b438d685bd61c423ccdc43cc96204eb230be8301b5e627b019fd975f"
+  end
+
   depends_on "rust" => :build
   depends_on "libsodium"
   depends_on "lmdb"
