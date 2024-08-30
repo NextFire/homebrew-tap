8de08b1ee201f672a4c3e0ff9ad0baad48211fb8
NextFire <git@yuru.moe>
2024-08-30 20:06:19 +0200

    Update apple-music-discord-rpc.rb

--- Formula/apple-music-discord-rpc.rb
+++ Formula/apple-music-discord-rpc.rb
@@ -1,8 +1,8 @@
 class AppleMusicDiscordRpc < Formula
   desc "Discord Rich Presence Client for the macOS Apple Music app and iTunes"
   homepage "https://github.com/NextFire/apple-music-discord-rpc"
-  url "https://github.com/NextFire/apple-music-discord-rpc/archive/refs/tags/0.12.0.tar.gz"
-  sha256 "235fc1ebcc16b54a5c012e086084a350b3477d85aa218b386e49555638ee0037"
+  url "https://github.com/NextFire/apple-music-discord-rpc/archive/refs/tags/0.12.1.tar.gz"
+  sha256 "2a179b2b60e1139c407dd9ba091351f6af12864c13e932013d341af29478133b"
   license "MIT"
   head "https://github.com/NextFire/apple-music-discord-rpc.git", branch: "main"
 
