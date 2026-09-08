b6d5ff9c398b48d6de3a17044a141688c43a96dc
NextFire <git@yuru.moe>
2026-09-08 22:22:20 +0200

    Update apple-music-discord-rpc.rb

--- Formula/apple-music-discord-rpc.rb
+++ Formula/apple-music-discord-rpc.rb
@@ -10,7 +10,13 @@ class AppleMusicDiscordRpc < Formula
 
   def install
     libexec.install "music-rpc.ts", "discord.ts", "jxa.ts"
-    bin.install_symlink libexec/"music-rpc.ts"
+    (bin/"music-rpc").write <<~SH
+      #!/bin/bash
+      exec "#{Formula["deno"].opt_bin}/deno" run \\
+        --allow-env --allow-run --allow-net --allow-read --allow-write \\
+        --unstable-kv \\
+        "#{libexec}/music-rpc.ts" "$@"
+    SH
   end
 
   service do
